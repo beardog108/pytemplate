04228c13ebf6e491b408982e97ae7105ded176ed
Kevin Froman <beardog@firemail.cc>
2019-07-15 17:22:48 -0500

    created python project template

---
+++ helloworld/__init__.py
@@ -0,0 +1,2 @@
+# You should rename the folder that this script is in to whatever the main package name is
+print("hello world")
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='modulename',
+      version='0.0.0',
+      description='Library desc',
+      author='Author Name',
+      author_email='author@example.com',
+      url='https://example.com/',
+      packages=['basename'],
+      install_requires=[],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+      ],
+     )
